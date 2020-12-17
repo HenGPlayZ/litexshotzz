08abd357df2f7312c06a3f4250a30499f6c63e84
HenGPlayZ <76139625+HenGPlayZ@users.noreply.github.com>
2020-12-17 14:55:07 +0700

    Update litexshotz.py

--- litexshotz.py
+++ litexshotz.py
@@ -5,8 +5,8 @@ client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.status.idle, activity=discord.Game ('litexshot.org'))
+    await client.change_presence(status=discord.status.idle, activity=discord.Game (litexshot•org'))
     print('Bot is ready.')
 
-client.run('Nzg5MDI5OTAwMzQ1NTQwNjQ5.X9sHWw.UC8MOxZtIIfzWITnLBvqN9hZFgA')
+client.run('NNzg5MDI5OTAwMzQ1NTQwNjQ5.X9sHWw.qbk63kOJWAeucH_rOFaUG81be9c')
 bot.run('token')
